59bfba7bd0039545a7dcf68cfa775b009c69dd5a
dmitriy365377 <tigr365377@gmail.com>
2020-04-01 00:05:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Вариант - 1
+
+document.body.children[1].lastElementChild.style.color = 'green';
+ 
+
+// Вариант - 2
+
+// const ul = document.getElementsByTagName('ul')[0]; 
+// ul.lastElementChild.style.color = 'green';
+
+
+ 
+ 
